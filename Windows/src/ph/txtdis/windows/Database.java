8ae916a0ffae7c7169d6cf6e54ecaa12b0243572
txtdis <vier_aquino@hotmail.com>
2013-06-23 12:50:37 +0800

    Changed version numbering

--- Windows/src/ph/txtdis/windows/Database.java
+++ Windows/src/ph/txtdis/windows/Database.java
@@ -1,4 +1,3 @@
-
 package ph.txtdis.windows;
 
 import java.sql.Connection;
@@ -6,31 +5,29 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-	public static String error  = "";
+	public static String error = "";
 	private static Database database = null;
 	private Connection connection = null;
-	
+
 	private Database() {
 	}
-	
+
 	public static Database getInstance() {
 		if (database == null) {
 			database = new Database();
 		}
 		return database;
 	}
-	
+
 	public Connection getConnection(String u, String p) {
 		if (connection == null) {
 			try {
-				connection = DriverManager.getConnection("" +
-						"jdbc:postgresql://" +
-						//"192.168.1.100" +						
+				connection = DriverManager.getConnection(""
+						+ "jdbc:postgresql://" +
+						// "192.168.1.100" +
 						"localhost" +
-						//"magnumstamaria.no-ip.org" +
-						":5432/txtdis", 
-						u, p
-						);
+						// "magnumstamaria.no-ip.org" +
+						":5432/txtdis", u, p);
 			} catch (SQLException e) {
 				e.printStackTrace();
 				error = e.toString();
@@ -45,7 +42,8 @@ public class Database {
 
 	public void closeConnection() {
 		try {
-			if (connection != null) connection.close();
+			if (connection != null)
+				connection.close();
 			connection = null;
 		} catch (SQLException e) {
 			e.printStackTrace();
